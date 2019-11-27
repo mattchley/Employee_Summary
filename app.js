4cad703ff633798def8308668d953baa4b2b6b16
mattchley <mattchley@gmail.com>
2019-11-27 11:56:12 -0800

    startsEmployeeObj

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-const Employee = require("./lib/Employee");
+const Employee = require('./lib/employee')
+const Manager = require('./lib/manager')
+const Engineer = require('./lib/engineer')
+const Intern = require('./lib/intern');
 
 // Initialize a new Employee Object
 const worker = new Employee();
 
-// Start adding workers
-worker.init();
+// tree
+async function init(){
+    try{
+        const fullName = await worker.getName();
+        const id = await worker.getId();
+        const email = await worker.getEmail();
+        const role = await worker.getRole();
+        
+    } catch (err) {
+        console.log(err);
+      }
+};
+
+init();
+
